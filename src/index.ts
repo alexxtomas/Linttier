bb63fbedfd7f4eb015e8c32713329489d949eb95
alexxtomas <atomasllimos@gmail.com>
2023-06-27 12:05:19 +0200

    feat: Handle empty package.json and refactor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,48 @@
 #!/usr/bin/env node
 
 import { parseArgs } from 'node:util'
-
+import { readdirSync } from 'node:fs'
+import { LinttierError } from './entities/error.js'
 const FRAMEWORKS = ['Next', 'React', 'Vanilla']
 const TEMPLATES = ['TypeScript', 'JavaScript']
+
+interface HandleArgsErrorsProps {
+	framework: string | undefined
+	template: string | undefined
+}
+
+function handleArgsErrors({ framework, template }: HandleArgsErrorsProps) {
+	if (typeof framework === 'undefined' || typeof template === 'undefined') {
+		throw new LinttierError({
+			name: 'INVALID_ARGUMENTS',
+			message: `Missing ${framework ? 'template' : 'framework'} argument`
+		})
+	}
+	if (!FRAMEWORKS.includes(framework)) {
+		throw new LinttierError({
+			name: 'INVALID_ARGUMENTS',
+			message: `Invalid framework '${framework}'.\nValid options:\n\t${FRAMEWORKS.join(
+				',\n\t'
+			)}`
+		})
+	}
+	if (!TEMPLATES.includes(template)) {
+		throw new LinttierError({
+			name: 'INVALID_ARGUMENTS',
+			message: `Invalid template '${template}'.\nValid options:\n\t${TEMPLATES.join(
+				',\n\t'
+			)}`
+		})
+	}
+
+	const files = readdirSync(process.cwd())
+	if (!files.includes('package.json')) {
+		throw new LinttierError({
+			name: 'INVALID_PACKAGE_JSON',
+			message: 'No "package.json" found in current directory'
+		})
+	}
+}
 
 export function handleArgs() {
 	try {
@@ -23,28 +62,33 @@ export function handleArgs() {
 			},
 			strict: true
 		})
+		try {
+			handleArgsErrors({ framework, template })
+		} catch (err) {
+			if (err instanceof LinttierError) {
+				console.error(err.message)
+			} else {
+				console.error(
+					'Unknown error, please try again. It it still does not work, please open an issue on GitHub.\nhttps://github.com/alexxtomas/Linttier/issues/new'
+				)
+			}
+			return
+		}
 
-		if (typeof framework === 'undefined' || typeof template === 'undefined') {
-			throw new Error(
-				`Missing ${framework ? 'template' : 'framework'} argument`
+		const _framework = framework as string
+		const _template = template as string
+
+		console.log(
+			`Creating ${_framework} ${_template} ESLint and Prettier config...`
+		)
+	} catch (err) {
+		if (err instanceof Error) {
+			console.error(err.message)
+		} else {
+			console.error(
+				'Unknown error, please try again. It it still does not work, please open an issue on GitHub.\nhttps://github.com/alexxtomas/Linttier/issues/new'
 			)
 		}
-		if (!FRAMEWORKS.includes(framework)) {
-			throw new Error(
-				`Invalid framework '${framework}'.\nValid options:\n\t${FRAMEWORKS.join(
-					',\n\t'
-				)}`
-			)
-		}
-		if (!TEMPLATES.includes(template)) {
-			throw new Error(
-				`Invalid template '${template}'.\nValid options:\n\t${TEMPLATES.join(
-					',\n\t'
-				)}`
-			)
-		}
-	} catch (e: any) {
-		console.error(e.message)
 	}
 }
 
