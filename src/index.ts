4ff11ceb575507860d414348822aa0b4ba70e3a7
alexxtomas <atomasllimos@gmail.com>
2023-06-27 06:55:46 +0200

    feat: Create handleArgs function

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,51 @@
 #!/usr/bin/env node
 
 import { parseArgs } from 'node:util'
-import { type Options } from './types'
 
 const FRAMEWORKS = ['Next', 'React', 'Vanilla']
 const TEMPLATES = ['TypeScript', 'JavaScript']
 
-try {
-	const {
-		values: { framework, template }
-	} = parseArgs({
-		args: process.argv.slice(2),
-		options: {
-			framework: {
-				type: 'string',
-				short: 'f'
+export function handleArgs() {
+	try {
+		const {
+			values: { framework, template }
+		} = parseArgs({
+			args: process.argv.slice(2),
+			options: {
+				framework: {
+					type: 'string',
+					short: 'f'
+				},
+				template: {
+					type: 'string',
+					short: 't'
+				}
 			},
-			template: {
-				type: 'string',
-				short: 't'
-			}
-		},
-		strict: true
-	})
+			strict: true
+		})
 
-	if (typeof framework === 'undefined' || typeof template === 'undefined') {
-		throw new Error(`Missing ${framework ? 'template' : 'framework'} argument`)
+		if (typeof framework === 'undefined' || typeof template === 'undefined') {
+			throw new Error(
+				`Missing ${framework ? 'template' : 'framework'} argument`
+			)
+		}
+		if (!FRAMEWORKS.includes(framework)) {
+			throw new Error(
+				`Invalid framework '${framework}'.\nValid options:\n\t${FRAMEWORKS.join(
+					',\n\t'
+				)}`
+			)
+		}
+		if (!TEMPLATES.includes(template)) {
+			throw new Error(
+				`Invalid template '${template}'.\nValid options:\n\t${TEMPLATES.join(
+					',\n\t'
+				)}`
+			)
+		}
+	} catch (e: any) {
+		console.error(e.message)
 	}
-	if (!FRAMEWORKS.includes(framework)) {
-		throw new Error(
-			`Invalid framework '${framework}'.\nValid options:\n\t${FRAMEWORKS.join(
-				',\n\t'
-			)}`
-		)
-	}
-	if (!TEMPLATES.includes(template)) {
-		throw new Error(
-			`Invalid template '${template}'.\nValid options:\n\t${TEMPLATES.join(
-				',\n\t'
-			)}`
-		)
-	}
-} catch (e: any) {
-	console.error(e.message)
 }
+
+handleArgs()
